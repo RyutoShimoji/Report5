a366a83dcbd1f744f1c4b65e83ec0693117bdf61
Ryuto Shimoji <e205755@ie.u-ryukyu.ac.jp>
2020-12-24 16:51:43 +0900

    seconds commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205755/report5.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205755/report5.java
@@ -2,7 +2,13 @@ package jp.ac.uryukyu.ie.e205755;
 public class report5 {
     public static void main(String[] args){
         String str = null;
-        System.out.println(str.length());
+        try{
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointerException");
+            System.out.println(e.getMessage());
+        } 
+
     }
 
 
