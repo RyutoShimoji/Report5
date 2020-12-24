8f7d67d083f8cd514836b6fe876a41f176fd3e7a
Ryuto Shimoji <e205755@ie.u-ryukyu.ac.jp>
2020-12-24 16:35:48 +0900

    first commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e205755/report5.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e205755; 
+public class report5 {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+
+
+}
